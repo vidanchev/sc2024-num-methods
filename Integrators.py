24b32692d5142de1f90607fab68e5f94c88e6d84
viktor.d <viktor.d@endurosat.com>
2024-08-07 11:52:01 +0300

    Added Verlet method for the oscillator and fixed some more labels

--- Integrators.py
+++ Integrators.py
@@ -1,5 +1,22 @@
 import numpy as np
 
+# Right-Hand-Side function which allows us to abstract the forces
+# for x' = rhs_x & v' = rhs_v -> you pass it x & v at some time and it gives you RHS
+# Inputs: 
+# - k spring const [N/m]
+# - m spring mass [kg]
+# - x position [m]
+# - v velocity [m/sec]
+# Outputs:
+# - rhs_x [m/sec]
+# - rhs_v [m/sec^2]
+def rhs_osc( k , m , x , v ):
+
+    rhs_x = v
+    rhs_v = - k/m * x
+
+    return rhs_x , rhs_v
+
 # Integrate Harmonic oscillator through Euler's method
 # Inputs:
 # - k -> spring constant [N/m]
@@ -37,10 +54,68 @@ def Euler_Oscillator( k , m , x_0 , v_0 , t_params ):
 
     # Integration loop -> go over each time step and extrapolate the next
     for i in range( 0 , Npoints - 1 ):
+        # NOTE: THIS IS BAD, NEVER HARDCODE YOUR RHS
         # x_(i+1) = x_(i) + v_(i)*dt
-        x_arr[ i + 1 ] = x_arr[ i ] + v_arr[ i ]*dt
+        #x_arr[ i + 1 ] = x_arr[ i ] + v_arr[ i ]*dt
         # v_(i+1) = v_(i) + f_(i)*dt, where f_(i) = -k/m*x_(i) for oscillator
-        v_arr[ i + 1 ] = v_arr[ i ] + ( - k / m )*x_arr[ i ]*dt
+        #v_arr[ i + 1 ] = v_arr[ i ] + ( - k / m )*x_arr[ i ]*dt
+        rhs_x, rhs_v = rhs_osc( m , k , x_arr[ i ] , v_arr[ i ] )
+        x_arr[ i + 1 ] = x_arr[ i ] + rhs_x*dt
+        v_arr[ i + 1 ] = v_arr[ i ] + rhs_v*dt
+
+    # My x_arr and v_arr are populated with the solution - return them
+    return t_arr , x_arr , v_arr
+
+
+# Integrate Harmonic oscillator through Verlet's method
+# Inputs:
+# - k -> spring constant [N/m]
+# - m -> mass [kg]
+# - x_0 -> initial position [m]
+# - v_0 -> initial velocity [m/sec]
+# - t_params[ 3 ] -> [ t_i , t_f , Npoints ] 
+# -- initial time (t_i) in [sec]
+# -- final time (t_f) in [sec]
+# -- Number of points Npoints [-]
+# Outputs:
+# - t_arr[ Npoints ] -> time array from t_i to t_f in [sec]
+# - x_arr[ Npoints ] -> position solution in [m]
+# - v_arr[ Npoints ] -> velocity solution in [m] 
+def Verlet_Oscillator( k , m , x_0 , v_0 , t_params ):
+
+    # Take out the initial and final time (1st and 2nd element of t_params)
+    t_i = t_params[ 0 ]
+    t_f = t_params[ 1 ]
+    # Take out the number of points which is 3d element in the t_params
+    Npoints = t_params[ 2 ]
+    # Compute the time step dt 
+    dt = ( t_f - t_i )/( Npoints - 1 ) 
+    # NOTE: Intervals are 1 less tha nnumber of points -> | | | has 3 points, 2 interfvals
+
+    # Create empty arrays which will hold the solution
+    x_arr = np.zeros( Npoints )
+    v_arr = np.zeros( Npoints )
+    # Create the time array
+    t_arr = np.linspace( t_i , t_f , Npoints )
+
+    # Assign the initial position and velocity
+    x_arr[ 0 ] = x_0
+    v_arr[ 0 ] = v_0
+
+    # Integration loop -> go over each time step and extrapolate the next
+    for i in range( 0 , Npoints - 1 ):
+        # Compute RHS in the first point (i)
+        rhs_x, rhs_v = rhs_osc( m , k , x_arr[ i ] , v_arr[ i ] )
+        # Compute half-velocity step -> intermediate step
+        v_half = v_arr[ i ] + rhs_v*dt/2.0 # NOTE: dt/2 -> half-step
+        # Compute RHS in the intermediate point (i+1/2)
+        rhs_x, rhs_v = rhs_osc( m , k , x_arr[ i ] , v_half )
+        # Compute full position step based on v_half
+        x_arr[ i + 1 ] = x_arr[ i ] + rhs_x*dt
+        # Compute RHS in the last point (i+1)
+        rhs_x, rhs_v = rhs_osc( m , k , x_arr[ i + 1 ] , v_half )
+        # Compute the full velocity step based on v_half and x[ i + 1 ]
+        v_arr[ i + 1 ] = v_half + rhs_v*dt/2.0
 
     # My x_arr and v_arr are populated with the solution - return them
     return t_arr , x_arr , v_arr
--- main.py
+++ main.py
@@ -10,19 +10,23 @@ if __name__ == "__main__":
     m = 1.0 # Mass [kg]
     x_0 = 1.0 # Initial position [m]
     v_0 = 0.0 # Initial speed [m/sec]
-    Npoints = 10000 # Number of points
+    Npoints = 1000 # Number of points
     t_params = [ 0.0 , # t_i 
                  4.0*np.pi , # t_f
                  Npoints ]
-
-    t_arr, x_arr, v_arr = Euler_Oscillator( k , m , x_0 , v_0 , t_params )
-
+    
+    # Obtain results with the Euler method 
+    t_arr, x_eul, v_eul = Euler_Oscillator( k , m , x_0 , v_0 , t_params )
+    # Obtain results with the Verlet method 
+    t_arr, x_ver, v_ver = Verlet_Oscillator( k , m , x_0 , v_0 , t_params )
+    # Real solution to compare with
     x_real = np.cos( np.sqrt( k/m )*t_arr )
 
     # 2D Plot of position vs time
     fig, ax = plt.subplots()
-    ax.plot( t_arr , x_arr , color = "red" , linestyle = "dashed" , label = r"simulated X" , linewidth = 4 )
-    ax.plot( t_arr , x_real , color = "green" , linestyle = "solid" , label = r"real X" , linewidth = 4 )
+    ax.scatter( t_arr , x_eul , color = "red" , linestyle = "dashed" , label = r"Euler sim" , linewidth = 4 )
+    ax.scatter( t_arr , x_ver , color = "green" , linestyle = "dashed" , label = r"Verlet sim" , linewidth = 4 )
+    ax.plot( t_arr , x_real , color = "blue" , linestyle = "solid" , label = r"real X" , linewidth = 2 )
 
     ax.set_xlabel( r"Time [sec]" )
     ax.set_ylabel( r"X [m]" )
@@ -31,18 +35,25 @@ if __name__ == "__main__":
     plt.grid( True )
     plt.show()
 
-    err_arr = np.zeros( Npoints )
+    # Create error arrays for Euler and Verlet to compare [%]
+    err_eul = np.zeros( Npoints )
+    err_ver = np.zeros( Npoints )
+
     for i in range( 0 , Npoints ):
-        err_arr[ i ] = np.abs( ( x_real[ i ] - x_arr[ i ] )/x_real[ i ] )*100.0
+        err_eul[ i ] = np.abs( ( x_real[ i ] - x_eul[ i ] )/x_real[ i ] )*100.0
+        err_ver[ i ] = np.abs( ( x_real[ i ] - x_ver[ i ] )/x_real[ i ] )*100.0
 
     # 2D Plot example
     fig, ax = plt.subplots()
-    ax.plot( t_arr , err_arr , color = "red" , linestyle = "dashed" , label = r"X error" , linewidth = 4 )
+    ax.plot( t_arr , err_eul , color = "red" , linestyle = "dashed" , label = r"Euler err" , linewidth = 4 )
+    ax.plot( t_arr , err_ver , color = "green" , linestyle = "dashed" , label = r"Verlet err" , linewidth = 4 )
+
 
     ax.set_xlabel( r"Time [sec]" )
-    ax.set_ylabel( r"X [m]" )
+    ax.set_ylabel( r"Error in [%]" )
 
     ax.legend( loc = "upper right" )
     plt.grid( True )
     plt.show()
-    print( "Max error = " + str( max( err_arr ) ) + " [%]" )
+    print( "Max error for Euler = " + str( max( err_eul ) ) + " [%]" )
+    print( "Max error for Verlet = " + str( max( err_ver ) ) + " [%]" )
